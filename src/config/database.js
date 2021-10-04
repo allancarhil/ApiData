b8fab09a51c552469cd5cd98d93fddd304c729d9
Pedro <pedro_penna22@hotmail.com>
2021-10-04 20:11:53 -0300

    adicionando api

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ module.exports = {
     dialect: 'mysql',
     host: '127.0.0.1',
     username: 'root',
-    password: '12345',
+    password: '123456',
     database: 'node',
     define: {
         timestamps: true,
